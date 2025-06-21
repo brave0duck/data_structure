a6e0a19461342088ca827b0c2f13c7f041fae39b
duckwoo <newkemp@naver.com>
2025-06-21 23:41:12 +0900

    LinkedList 0.01ver

--- 02/LinkedList.c
+++ 02/LinkedList.c
@@ -2,21 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedefine struct linked_list_node_type{
+typedef struct LinkedListNodeType{
     int data;
-    linked_list_node* p_node;
-}linked_list_node;
+    struct LinkedListNodeType * pLink;
+} LinkedListNode;
 
-typedef struct linked_list_type{
-    int count;
-    linked_list_node p_start;
-}linked_list;
+typedef struct LinkedListType{
+    int currentCount;
+    LinkedListNode headerNode;
+} LinkedList;
 
-linked_list* createLinkedList(void){
-    
-}
+LinkedList* createLinkedList();
+int getLinkedListData(LinkedList* pList, int position);
+LinkedListNode* getLinkedListPointer(LinkdedList* pList, int position);
 int main(void){
 
-
     return 0;
+}
+
+LinkedList* createLinkedList(){
+    LinkedList* pReturn = (LinkedList*)malloc(sizeof(LinkedList));
+    memset(pReturn, 0, sizeof(LinkedList));
+    return pReturn;
+}
+
+int getLinkedListData(LinkedList* pList, int position){
+    LinkedListNode* pCurrentNode = &(pList->headerNode);
+
+    for(int i=1; i<=position;i++)
+        pCurrentNode = pCurrentNode->pLink;
+    return pCurrentNode->data;
+}
+LinkedListNode* getLinkedListPointer(LinkdedList* pList, int position){
+    
+    LinkedListNode* pCurrentNode = &(pList->headerNode);
+
+    for(int i=1; i<=position;i++)
+        pCurrentNode = pCurrentNode->pLink;
+    return pCurrentNode;
+}
+
+int addLinkedListData(LinkedList* pList, int position, int data){
+    LinkedListNode* pBefore, *pAfter, *pNew;
+    if(pList->currentCount >= position){        //position의 주소 + position-1의 주소가 필요하다
+        pBefore = getLinkedListPointer(pList,position-1);
+        pAfter = getLinkedListPointer(pList,position);
+
+        pNew = calloc(1,sizeof(LinkedListNode));
+        pNew->data = data;
+        pNew->pLink = pAfter;
+
+        pBefore->pLink = &pNew;
+    }
+    else{
+        
+        pAfter = calloc(1,sizeof(LinkedListNode));
+        pAfter->data = data;
+        pAfter->pLink = NULL;
+
+        pBefore = getLinkedListPointer(pList, pList->currentCount);
+        pBefore->pLink = &pAfter;
+    }
+
+    pList->currentCount++;
 }
