61ee287dd5c9ee184b19fc59182fb0628059353c
brave0duck <newkemp@naver.com>
2025-07-03 14:36:21 +0900

    CIRCLE QUEUE

---
+++ 07/CircleQueue.h
@@ -0,0 +1,28 @@
+#ifndef _CIRCLE_QUEUE_
+#define _CIRCLE_QUEUE_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct n{
+    int data;
+}Node;
+
+typedef struct q{
+    
+    int front;
+    int end;
+    Node* pNode;
+}Queue;
+
+Queue* Create(int size);
+void EnQueue(Queue* pQ);
+Node* DeQueue(Queue* pQ);
+int isEmpty(Queue* pQ);
+int isFull(Queue* pQ);
+int Delete(Queue * pQ);
+int CountQueue(Queue* pQ);
+int PrintQueue(Queue* pQ);
+
+#endif
