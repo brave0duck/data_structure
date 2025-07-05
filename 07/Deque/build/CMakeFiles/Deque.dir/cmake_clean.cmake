40b899867d18bd0c4a7327edaa45e2f578b13638
brave0duck <newkemp@naver.com>
2025-07-05 15:08:37 +0900

    cmake use

---
+++ 07/Deque/build/CMakeFiles/Deque.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deque.dir/link.d"
+  "CMakeFiles/Deque.dir/Deque.c.o"
+  "CMakeFiles/Deque.dir/Deque.c.o.d"
+  "CMakeFiles/Deque.dir/main.c.o"
+  "CMakeFiles/Deque.dir/main.c.o.d"
+  "Deque"
+  "Deque.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Deque.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
